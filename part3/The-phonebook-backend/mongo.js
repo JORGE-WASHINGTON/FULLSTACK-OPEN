815f43e8c37510c7a88697605a4276a93f41bffd
Jorge Washington <jwoliveira18@gmail.com>
2021-10-26 21:41:00 -0300

    Exercises 3.12

--- part3/The-phonebook-backend/mongo.js
+++ part3/The-phonebook-backend/mongo.js
@@ -1,53 +1,46 @@
-/* const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-const url =
-  `mongodb+srv://fullstack:${password}@cluster0.gwznd.mongodb.net/note-app?retryWrites=true&w=majority`
+const url = `mongodb+srv://fullstack:${password}@cluster0.gwznd.mongodb.net/note-app?retryWrites=true&w=majority`;
 
-mongoose.connect(url)
+mongoose.connect(url);
 
 const contactSchema = new mongoose.Schema({
   name: String,
   number: String,
   date: Date,
-})
+});
 
-const Contact = mongoose.model('Contact', contactSchema)
+const Contact = mongoose.model("Contact", contactSchema);
 
-const contact = new Contact({
-  name: process.argv[3],
-  number: process.argv[4],
-  date: new Date()
-})
+if (process.argv.length === 5) {
+  const contact = new Contact({
+    name: process.argv[3],
+    number: process.argv[4],
+    date: new Date(),
+  });
 
-/* contact.save().then(result => {
-  console.log(`Added ${result.name} number ${result.number} to phonebook`);
-  mongoose.connection.close()
-}) */
-
-/* if (process.argv.length === 3) {
-  Contact
-    .find({})
-    .then(result => {
-      console.log('Phonebook:');
-      result.forEach(contact => {
-        console.log(`${contact.name} ${contact.number}`)
-      })
-      mongoose.connection.close()
-    }) 
-} */
-
-/* Note.find({}).then(result => {
-  result.forEach(note => {
-    console.log(note);
-  })
-  mongoose.connection.close()
-}) */
-
- */
+  contact.save().then((result) => {
+    console.log(`Added ${result.name} number ${result.number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else if (process.argv.length === 4) {
+  console.log("Please provide name and number");
+  mongoose.connection.close();
+} else if (process.argv.length === 3) {
+  Contact.find({}).then((result) => {
+    console.log("Phonebook:");
+    result.forEach((contact) => {
+      console.log(`${contact.name} ${contact.number}`);
+    });
+    mongoose.connection.close();
+  });
+}
